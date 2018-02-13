ad1aec5481b7310926509ec1c7bbfc19e5908ec8
Santeri Jouko Matias Loitomaa <santtinen3@gmail.com>
2018-02-13 16:15:54 +0200

    Update Kuningas.java

--- Kuningas.java
+++ Kuningas.java
@@ -4,10 +4,10 @@ package kuningaskuntaSimulaatio;
 Aiheemme on C. Peli.
 
 Peli alkaa muodostamalla kuningaskunnan, jossa pelaaja on kuningas. Pelaajana hallitset sukuja, joilla on riitoja ja suhteita toisiinsa.
-Sukuja on monenlaisia (maagisia, uskonnollisia, sotilaallisia... jne.). Suvuille tulee ongelmia joihin sinun tulee tehd‰ viisaita
-p‰‰tˆksi‰. P‰‰tˆksesi vaikuttaa resursseihin ja sukujen suhteisiin, joten valitse viisaasti.
+Sukuja on monenlaisia (maagisia, uskonnollisia, sotilaallisia... jne.). Suvuille tulee ongelmia joihin sinun tulee tehd√§ viisaita
+p√§√§t√∂ksi√§. P√§√§t√∂ksesi vaikuttaa resursseihin ja sukujen suhteisiin, joten valitse viisaasti.
 
-Peliss‰ on useita vuoroja joiden aikana p‰‰tˆksi‰ tehd‰‰n eri sukujen erilaisiin ongelmiin. Lopussa lasketaan pisteet kuinka "hyv‰"
+Peliss√§ on useita vuoroja joiden aikana p√§√§t√∂ksi√§ tehd√§√§n eri sukujen erilaisiin ongelmiin. Lopussa lasketaan pisteet kuinka "hyv√§"
 kuningaskunta saadaan aikaiseksi.
 */
 
@@ -21,7 +21,7 @@ public class Kuningas{ //Pelaaja
   public ArrayList<Suku> suvut;
   public static Scanner vastaus;
   
-  private static void vuorokierto(){ // Koko peli t‰‰ll‰
+  private static void vuorokierto(){ // Koko peli t√§√§ll√§
     raha = 100;
     ruoka = 100;
     for(int i = 0; i < vuorot; i++){
@@ -44,7 +44,7 @@ public class Kuningas{ //Pelaaja
   public static void main(String[] args){
     //prologi
     vastaus = new Scanner(System.in);
-    System.out.println("Tervetuloa pelaamaan Kuningaskunta Simulaattoria! Mik‰ on nimenne ja tittelinne teid‰n ylh‰isyytenne?");
+    System.out.println("Tervetuloa pelaamaan Kuningaskunta Simulaattoria! Mik√§ on nimenne ja tittelinne teid√§n ylh√§isyytenne?");
     nimi = vastaus.next();
     System.out.println("Kuinka monta vuotta aiot hallita? (Vuodessa tulee 12 ongelmaa!)");
     vuorot = vastaus.nextInt() * 12;
@@ -53,7 +53,7 @@ public class Kuningas{ //Pelaaja
     vuorokierto();
     
     //pelin lopetus
-    System.out.println("Valtakautesi on p‰‰ttynyt.");
+    System.out.println("Valtakautesi on p√§√§ttynyt.");
     tulostaPisteet();
   }
 
@@ -71,13 +71,13 @@ class Suku{
   private int aatelisuus;			//Aina arvot 0-3
   
   private int magia;				//Suvun maagisuus (0-4)
-  private int sotilaallinen;		//Suvun sotilaallisuus (0-4)
-  private int uskonnollinen;		//Suvun uskonnollisuus (0-4)   ARVOT YHTEENSƒ 4 PER SUKU!
+  private int sotilaallinen;			//Suvun sotilaallisuus (0-4)
+  private int uskonnollinen;			//Suvun uskonnollisuus (0-4)   ARVOT YHTEENS√Ñ 4 PER SUKU!
   private int kauppias;				//Suvun kauppiaisuus (0-4)
   private int maalainen;			//Suvun maallisuus (0-4)
   
   public void annaTiedot(){
-    //Anna edustaja, suvun nimi, aatelisuus ja muut ominaisuudet pisteineen. Annetaan aina kun vuorossa esitell‰‰n ongelma.
+    //Anna edustaja, suvun nimi, aatelisuus ja muut ominaisuudet pisteineen. Annetaan aina kun vuorossa esitell√§√§n ongelma.
   }
   public Suku(){
   }
@@ -86,9 +86,9 @@ class Suku{
 class Ongelma{
   private String nimi;
   private String selitys;
-  private ArrayList<Suku> kohteet; //Ensimm‰inen kohde on esittelija, 
+  private ArrayList<Suku> kohteet; //Ensimm√§inen kohde on esittelija, 
   private ArrayList<Paatos> paatokset;
-    // Mahdollisesti vaikeusastelis‰ys n‰ille ajan myˆt‰
+    // Mahdollisesti vaikeusastelis√§ys n√§ille ajan my√∂t√§
   public void tulosta(){
     
   }
