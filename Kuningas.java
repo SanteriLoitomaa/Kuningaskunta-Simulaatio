4eb18e4f7e7af8570372152d3614fa027737879a
Santeri Jouko Matias Loitomaa <santtinen3@gmail.com>
2018-02-13 16:23:10 +0200

    Update Kuningas.java

--- Kuningas.java
+++ Kuningas.java
@@ -1,16 +1,5 @@
 package kuningaskuntaSimulaatio;
 
-/*
-Aiheemme on C. Peli.
-
-Peli alkaa muodostamalla kuningaskunnan, jossa pelaaja on kuningas. Pelaajana hallitset sukuja, joilla on riitoja ja suhteita toisiinsa.
-Sukuja on monenlaisia (maagisia, uskonnollisia, sotilaallisia... jne.). Suvuille tulee ongelmia joihin sinun tulee tehdä viisaita
-päätöksiä. Päätöksesi vaikuttaa resursseihin ja sukujen suhteisiin, joten valitse viisaasti.
-
-Pelissä on useita vuoroja joiden aikana päätöksiä tehdään eri sukujen erilaisiin ongelmiin. Lopussa lasketaan pisteet kuinka "hyvä"
-kuningaskunta saadaan aikaiseksi.
-*/
-
 import java.util.*;
 
 public class Kuningas{ //Pelaaja
