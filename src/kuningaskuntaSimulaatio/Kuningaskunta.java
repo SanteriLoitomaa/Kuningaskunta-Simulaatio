adb8cb323f321fc2c9878165b74cad067bddef34
Santeri Jouko Matias Loitomaa <santtinen3@gmail.com>
2018-02-14 21:14:31 +0200

    Add files via upload

--- src/kuningaskuntaSimulaatio/Kuningaskunta.java
+++ src/kuningaskuntaSimulaatio/Kuningaskunta.java
@@ -9,21 +9,26 @@ public class Kuningaskunta{
     //prologi
     Scanner vastaus = new Scanner(System.in);
     System.out.println("Tervetuloa pelaamaan Kuningaskunta Simulaattoria! Miten haluatte ett‰ kutsun teit‰, teid‰n ylh‰isyytenne? (nimi ja titteli)");
-    String nimi = vastaus.next();
+    String nimi = vastaus.nextLine();
     System.out.println("Kuinka monta vuotta aiot hallita? (Joka kuukausi tapahtuu jotain!)");
-    int vuorot = vastaus.nextInt() * 12;
-    Kuningas kunkku = new Kuningas(nimi, vuorot);
+    while(!vastaus.hasNextInt()){
+    	vastaus.next();
+	}
+    int vuorot = vastaus.nextInt();
+    vuorot = vuorot * 12;
+	Kuningas kunkku = new Kuningas(nimi, vuorot);
     meillaOnOngelmia(kunkku, vuorot);
-
+    
     //peli alkaa
     kunkku.vuorokierto();
   }
-  
-  public static void meillaOnOngelmia(Kuningas kunkku, int vuorot){ //Luo kaikki pelin ongelmat, pit‰‰ kutsua sukujen luonnin j‰lkeen.
+
+public static void meillaOnOngelmia(Kuningas kunkku, int vuorot){ //Luo kaikki pelin ongelmat, pit‰‰ kutsua sukujen luonnin j‰lkeen.
 		ArrayList<Paatos> paatokset = new ArrayList<Paatos>();
-		paatokset.add(new Paatos(new Vaatimus[]{new Vaatimus(Tyyppi.Raha, kunkku.annaSukujenLKM()*5, null)}, new Seuraus[]{new Seuraus(Tyyppi.Raha, -kunkku.annaSukujenLKM()*5, null), new Seuraus(Tyyppi.Sukusuhde, 20, kunkku.suvut)}));
+		paatokset.add(new Paatos(new Vaatimus[]{new Vaatimus(Tyyppi.Raha, kunkku.annaSukujenLKM()*5, null)}, new Seuraus[]{
+				new Seuraus(Tyyppi.Raha, -kunkku.annaSukujenLKM()*5, null), new Seuraus(Tyyppi.Sukusuhde, 20, kunkku.suvut)}));
     
-		kunkku.ongelmat.add(new Ongelma("Maanj‰ristys", "Jumalat ovat vihaisia meille. Kaikkia sukuja on kohdannut onnettomuus ja heid‰n tilansa"
-			+ " ovat kokeneet suurta vahinkoa. Miten toimimme?", kunkku.suvut, paatokset));
+		kunkku.ongelmat.add(new Ongelma("Maanj‰ristys", "Jumalat ovat vihaisia meille. Kaikkia sukuja on kohdannut onnettomuus ja heid‰n "
+				+ "tilansa ovat kokeneet suurta vahinkoa. Miten toimimme?", kunkku.suvut, paatokset));
   }
 }
