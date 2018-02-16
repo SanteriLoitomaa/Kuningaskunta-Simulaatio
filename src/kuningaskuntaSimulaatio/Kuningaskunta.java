98ae810b9a42836cb27175cd85e10c79c99444d6
Santtinen <santtinen3@gmail.com>
2018-02-16 15:38:44 +0200

    Ongelmien parsiminen

--- src/kuningaskuntaSimulaatio/Kuningaskunta.java
+++ src/kuningaskuntaSimulaatio/Kuningaskunta.java
@@ -2,36 +2,40 @@ package kuningaskuntaSimulaatio;
 
 import java.util.*;
 
-public class Kuningaskunta{
-  
-  public static void main(String[] args){
-    
-    //prologi
-    Scanner vastaus = new Scanner(System.in);
-    System.out.println("Tervetuloa pelaamaan Kuningaskunta Simulaattoria! Miten haluatte että kutsun teitä, teidän ylhäisyytenne? (nimi ja titteli)");
-    String nimi = vastaus.nextLine();
-    System.out.println("Kuinka monta vuotta aiot hallita? (Joka kuukausi tapahtuu jotain!)");
-    while(!vastaus.hasNextInt()){
-    	vastaus.next();
+public class Kuningaskunta {
+
+	public static void main(String[] args) {
+
+		// prologi
+		Scanner vastaus = new Scanner(System.in);
+		System.out.println(
+				"Tervetuloa pelaamaan Kuningaskunta Simulaattoria! Miten haluatte että kutsun teitä, teidän ylhäisyytenne? (nimi ja titteli)");
+		String nimi = vastaus.nextLine();
+		System.out.println("Kuinka monta vuotta aiot hallita? (Joka kuukausi tapahtuu jotain!)");
+		while (!vastaus.hasNextInt()) {
+			vastaus.next();
+		}
+		int vuorot = vastaus.nextInt();
+		vuorot = vuorot * 12;
+		Kuningas kunkku = new Kuningas(nimi, vuorot);
+		meillaOnOngelmia(kunkku, vuorot);
+
+		// peli alkaa
+		kunkku.vuorokierto();
 	}
-    int vuorot = vastaus.nextInt();
-    vuorot = vuorot * 12;
-	Kuningas kunkku = new Kuningas(nimi, vuorot);
-    meillaOnOngelmia(kunkku, vuorot);
-    
-    //peli alkaa
-    kunkku.vuorokierto();
-  }
-  
-//Luo kaikki pelin ongelmat, pitää kutsua sukujen luonnin jälkeen.
-public static void meillaOnOngelmia(Kuningas kunkku, int vuorot){ 
+
+	// Luo kaikki pelin ongelmat, pitää kutsua sukujen luonnin jälkeen.
+	public static void meillaOnOngelmia(Kuningas kunkku, int vuorot) {
 		ArrayList<Paatos> paatokset = new ArrayList<Paatos>();
-		
-		paatokset.add(new Paatos(new Vaatimus[]{new Vaatimus(Tyyppi.Raha, kunkku.annaSukujenLKM()*5, null)}, new Seuraus[]{
-				new Seuraus(Tyyppi.Raha, -kunkku.annaSukujenLKM()*5, null), new Seuraus(Tyyppi.Sukusuhde, 20, kunkku.suvut)},
-					"Voin auttaa teitä korjaustöissä viidellä kullalla per suku."));
-				
-		kunkku.ongelmat.add(new Ongelma("Maanjäristys", "Jumalat ovat vihaisia meille. Kaikkia sukuja on kohdannut onnettomuus ja heidän "
-				+ "tilansa ovat kokeneet suurta vahinkoa. Miten toimimme?", kunkku.suvut, paatokset));
-  }
+
+		paatokset.add(new Paatos(new Vaatimus[] { new Vaatimus(Tyyppi.RAHA, kunkku.annaSukujenLKM() * 5, null) },
+				new Seuraus[] { new Seuraus(Tyyppi.RAHA, -kunkku.annaSukujenLKM() * 5, null),
+						new Seuraus(Tyyppi.SUKUSUHDE, 20, kunkku.suvut) },
+				"Voin auttaa teitä korjaustöissä viidellä kullalla per suku."));
+
+		kunkku.ongelmat.add(new Ongelma("Maanjäristys",
+				"Jumalat ovat vihaisia meille. Kaikkia sukuja on kohdannut onnettomuus ja heidän "
+						+ "tilansa ovat kokeneet suurta vahinkoa. Miten toimimme?",
+				kunkku.suvut.get(0), paatokset));
+	}
 }
--- src/kuningaskuntaSimulaatio/Ongelma.java
+++ src/kuningaskuntaSimulaatio/Ongelma.java
@@ -2,65 +2,121 @@ package kuningaskuntaSimulaatio;
 
 import java.util.*;
 
-public class Ongelma{
-  private String nimi;
-  private String selitys;
-  private ArrayList<Suku> kohteet; //Ensimmäinen kohde on esittelija, 
-  private ArrayList<Paatos> paatokset;
-    // Mahdollisesti vaikeusastelisäys näille ajan myötä
-  public void tulosta(){
-    
-  }
-  public Ongelma(String nimi, String selitys, ArrayList<Suku> kohteet, ArrayList<Paatos> paatokset){ //(Sisälsi alunperin intin) Ongelman luonnin jälkeen pitää ne lisätä arraylistiin.
-    this.nimi = nimi;
-    this.selitys = selitys;
-    this.kohteet = kohteet;
-    this.paatokset = paatokset;
-  }
+public class Ongelma {
+	private String nimi;
+	private String selitys;
+	private Suku esittelijaSuku;
+	private ArrayList<Paatos> paatokset;
+
+	// Ongelman luonnin jälkeen pitää ne lisätä arraylistiin.
+	public Ongelma(String nimi, String selitys, Suku esittelijaSuku, ArrayList<Paatos> paatokset) {
+		this.nimi = nimi;
+		this.selitys = selitys;
+		this.esittelijaSuku = esittelijaSuku;
+		this.paatokset = paatokset;
+	}
+
+	// Mahdollisesti vaikeusastelisäys näille ajan myötä
+	public void tulosta(Kuningas kunkku) {
+		for (Paatos p : paatokset) {
+			p.tulostaPaatosrivi(selitys, kunkku);
+		}
+	}
+
+	// Pelaajan valitsema päätös lähtee liikkeelle
+	public void valitsePaatos(int valinta, Kuningas kunkku) {
+		paatokset.get(valinta - 1).toteutaSeuraukset(kunkku);
+	}
 }
 
-class Paatos{
-  private Vaatimus[] v;
-  private Seuraus[] s;
-  public Paatos(Vaatimus[] v, Seuraus[] s, String viesti) {
-	this.v  = v;
-    this.s = s;
-  }
-  public void tulostaPaatosrivi(){
-    
-  }
-  public void toteutaSeuraukset(){
-    
-  }
+class Paatos {
+	private Vaatimus[] vaatimukset;
+	private Seuraus[] seuraukset;
+
+	public Paatos(Vaatimus[] v, Seuraus[] s, String viesti) {
+		this.vaatimukset = v;
+		this.seuraukset = s;
+	}
+
+	// Tulostaa mahdollisuudet
+	public void tulostaPaatosrivi(String selitys, Kuningas kunkku) {
+		int i = 1;
+		for (Vaatimus v : vaatimukset) {
+			if (v.tarkistaVaatimus(kunkku)) {
+				System.out.println(i + ". " + selitys);
+			}
+			i++;
+		}
+	}
+
+	// Toteuta paatos
+	public void toteutaSeuraukset(Kuningas kunkku) {
+		for (Seuraus s : seuraukset) {
+			s.toteuta(kunkku);
+		}
+	}
 }
 
-enum Tyyppi{
-	Raha,
-	Ruoka,
-	Sukusuhde
+enum Tyyppi {
+	RAHA, RUOKA, SUKUSUHDE
 }
 
-class Vaatimus{
-  private Tyyppi tyyppi;
-  private int arvo;
-  private ArrayList<Suku> kohde;
-  public Vaatimus(Tyyppi tyyppi, int arvo, ArrayList<Suku> kohde) {
+class Vaatimus {
+	private Tyyppi tyyppi;
+	private int arvo;
+	private ArrayList<Suku> kohde;
+
+	public Vaatimus(Tyyppi tyyppi, int arvo, ArrayList<Suku> kohde) {
 		this.tyyppi = tyyppi;
 		this.arvo = arvo;
-		if(kohde != null) this.kohde = kohde;
-  }
-  public boolean tarkistaVaatimus() {
+		if (kohde != null)
+			this.kohde = kohde;
+	}
+
+	// Tarkista mahdollisuus
+	public boolean tarkistaVaatimus(Kuningas kunkku) {
+		if (tyyppi == Tyyppi.RAHA) {
+			if (kunkku.annaRaha() >= this.arvo)
+				return true;
+		}
+		if (tyyppi == Tyyppi.RUOKA) {
+			if (kunkku.annaRuoka() >= this.arvo)
+				return true;
+		}
+		if (tyyppi == Tyyppi.SUKUSUHDE) {
+			for (Suku s : kohde) {
+				if (s.annaSuhdeKuninkaaseen() >= arvo)
+					return true;
+			}
+		}
 		return false;
-  }
+	}
 }
 
-class Seuraus{
-  private Tyyppi tyyppi;
-  private int arvo;
-  private ArrayList<Suku> kohde;
-  public Seuraus(Tyyppi tyyppi, int arvo, ArrayList<Suku> kohde) {
+class Seuraus {
+	private Tyyppi tyyppi;
+	private int arvo;
+	private ArrayList<Suku> kohde;
+
+	public Seuraus(Tyyppi tyyppi, int arvo, ArrayList<Suku> kohde) {
 		this.tyyppi = tyyppi;
 		this.arvo = arvo;
-		if(kohde != null) this.kohde = kohde;
-  }
+		if (kohde != null)
+			this.kohde = kohde;
+	}
+
+	// Tee muutokset
+	public void toteuta(Kuningas kunkku) {
+		if (tyyppi == Tyyppi.RAHA) {
+			kunkku.asetaRaha(kunkku.annaRaha() + this.arvo);
+		}
+		if (tyyppi == Tyyppi.RUOKA) {
+			kunkku.asetaRuoka(kunkku.annaRuoka() + this.arvo);
+		}
+		if (tyyppi == Tyyppi.SUKUSUHDE) {
+			for (Suku s : kohde) {
+				s.asetaSuhdeKuninkaaseen(s.annaSuhdeKuninkaaseen() + arvo);
+			}
+		}
+	}
 }
