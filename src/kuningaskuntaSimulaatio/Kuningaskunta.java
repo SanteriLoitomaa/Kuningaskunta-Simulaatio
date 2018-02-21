e270b7b61cdcb561fe708749385c14893ff965fe
Ashtaka97 <tommihheikkinen@gmail.com>
2018-02-21 14:35:42 +0200

    Eka ongelma

--- src/kuningaskuntaSimulaatio/Kuningaskunta.java
+++ src/kuningaskuntaSimulaatio/Kuningaskunta.java
@@ -36,7 +36,31 @@ public class Kuningaskunta {
 				new Seuraus[] { new Seuraus(Tyyppi.RAHA, -(kunkku.annaSukujenLKM() * 5), null),
 						new Seuraus(Tyyppi.SUKUSUHDE, 20, kunkku.suvut) },
 				"Voin auttaa teitä korjaustöissä viidellä kullalla per suku."));
+		
+		paatokset.add(new Paatos(new Vaatimus[] { new Vaatimus(Tyyppi.SUKUSUHDE, 1, /*kunkku.etsiSuosituinMagia().get(0)*/) },
+				new Seuraus[] { new Seuraus(Tyyppi.SUKUSUHDE, 10, kunkku.suvut),
+						new Seuraus(Tyyppi.SUKUSUHDE, -20, /*kunkku.etsiSuosituinUskonnollinen()*/),
+						new Seuraus(Tyyppi.SUKUSUHDE, 20, /*etsisuku(maaginen & uskonnollinen)*/),
+						/*Puuttuu vielä sukujen väliset muutokset*/},
+				"Kansa on selvästikkin käsittänyt jotain väärin. Annas kun selitän, mistä tämä johtui."));
 
+		Random r = new Random();
+		Suku x = kunkku.suvut.get(r.nextInt(kunkku.suvut.size()));
+		String neitsyenKoti = x.annaNimi();
+		ArrayList<Suku> uhri = new ArrayList<Suku>(Arrays.asList(x));
+		// Huumori mielessä jätimem mahdolliseksi tapattaa ehdottajan tyttären xD
+		
+		paatokset.add(new Paatos(new Vaatimus[] { new Vaatimus(Tyyppi.SUKUSUHDE, 1, /*kunkku.etsiSuosituinUskonnollinen().get(0)*/) },
+				new Seuraus[] { new Seuraus(Tyyppi.SUKUSUHDE, 10, kunkku.suvut),
+						new Seuraus(Tyyppi.SUKUSUHDE, -20, uhri),
+						new Seuraus(Tyyppi.SUKUSUHDE, 10, kunkku.suvut),
+						/*Puuttuu vielä sukujen väliset muutokset*/},
+				"Jos jumalat ovat tosiaan niin vihaisia meidän on uhrattava neitsyt " + neitsyenKoti + " suvulta mitä pikimmiten."));
+		
+		paatokset.add(new Paatos(new Vaatimus[] { new Vaatimus(Tyyppi.NULL, 0, null) },
+				new Seuraus[] { new Seuraus(Tyyppi.SUKUSUHDE, -20, kunkku.suvut)},
+				"En näe miten tämä ongelma koskee minua."));
+		
 		kunkku.ongelmat.add(new Ongelma("Maanjäristys",
 				"Jumalat ovat vihaisia meille. Kaikkia sukuja on kohdannut onnettomuus ja heidän "
 						+ "tilansa ovat kokeneet suurta vahinkoa. Miten toimimme?",
