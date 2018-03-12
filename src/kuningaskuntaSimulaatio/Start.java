96dbabadf98e1afa891b3d76bb65ed32508014b4
Santtinen <santtinen3@gmail.com>
2018-03-12 11:34:21 +0200

    .

--- src/kuningaskuntaSimulaatio/Start.java
+++ src/kuningaskuntaSimulaatio/Start.java
@@ -12,7 +12,6 @@ public class Start{
         if(console == null && !GraphicsEnvironment.isHeadless()){
         	String filename = Start.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6,
         			Start.class.getProtectionDomain().getCodeSource().getLocation().toString().length()-1) + "Kuningaskunta-simulaatio.jar";
-            System.out.println(filename);
             Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});
         }else{
             Kuningaskunta.main(new String[0]);
